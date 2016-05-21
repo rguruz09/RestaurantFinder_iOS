12b5d9c79916ab4cfb9f6288211987c46effb9b7
Savanigit config --global user.name S <savani.vishnoi@gmail.com>
2016-05-21 14:32:42 -0700

    Initial Commit

---
+++ MyRestaurantFinderIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyRestaurantFinderIOS
+//
+//  Created by Savani Vishnoi on 5/21/16.
+//  Copyright © 2016 Savani Vishnoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
