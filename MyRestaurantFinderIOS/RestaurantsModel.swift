95c222f444eed6ebb0f4c475c1c681b9b0221e1d
Shruti kangokar <shrutik@Shrutis-MacBook-Pro.local>
2016-05-22 16:19:56 -0700

    Added new controller for list and model data

--- MyRestaurantFinderIOS/RestaurantsModel.swift
+++ MyRestaurantFinderIOS/RestaurantsModel.swift
@@ -13,19 +13,60 @@ class RestaurantModel {
     
     var id: String
     var rating:Int
+    var is_claimed: Bool
+    var is_closed: Bool
+    var name: String
+    var image_url:String
+    var phone:String
+    var display_phone: String
+    var review_count: Int16
+    var categories: [String:String]
+    var rating_img_url:String
+    var snippet_text:String
+    var snippet_image_url:String
+    var address: String
+    var city: String
+    var country_code: String
+    var postal_code:String
+    var state_code:String
     
-    
-    init(id: String, is_claimed: Bool, is_closed: Bool, name: String,
-        image_url:String, phone:String, display_phone: String,
-        review_count: Int16, categories: [String:String],
-        rating: Int, rating_img_url:String,
-        snippet_text:String, snippet_image_url:String,
-        address: String, city: String, country_code: String,
-        postal_code:String, state_code:String ){
-            self.id = id
-            self.rating = rating
-            
+    init( id: String
+    ,rating:Int
+    ,is_claimed: Bool
+    , is_closed: Bool
+    , name: String
+    , image_url:String
+    , phone:String
+    , display_phone: String
+    , review_count: Int16
+    , categories: [String:String]
+    , rating_img_url:String
+    , snippet_text:String
+    , snippet_image_url:String
+    , address: String
+    , city: String
+    , country_code: String
+    , postal_code:String
+    , state_code:String){
+        self.address = address;
+        self.categories = categories;
+        self.city = city;
+        self.country_code = country_code;
+        self.id = id;
+        self.image_url = image_url;
+        self.is_claimed = is_claimed;
+        self.is_closed = is_closed;
+        self.name = name;
+        self.phone = phone;
+        self.postal_code = postal_code;
+        self.rating = rating;
+        self.state_code = state_code;
+        self.rating_img_url = rating_img_url;
+        self.review_count = review_count;
+        self.snippet_image_url = snippet_image_url;
+        self.snippet_text = snippet_text;
+        self.display_phone = display_phone;
+        
     }
-    
     
 }
